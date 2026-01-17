ef7520d4cb4ae4a730ea0710acf2d5da22450546
Hariel Santos <harielp.santos1@gmail.com>
2026-01-17 06:06:54 -0400

    esqueletoDoProjeto

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,11 @@
+import livro from '../../imagens/livro.png';
+
+export const Livros =[
+    {nome: 'liderança em desing', id: 1, src:livro},
+    {nome: 'criatividade', id: 2, src:livro},
+    {nome: 'inovação', id: 3, src:livro},
+    {nome: 'gestão de pessoas', id: 4, src:livro},
+    {nome: 'produtividade', id: 5, src:livro},
+    {nome: 'tecnologia', id: 6, src:livro}, 
+    
+]
