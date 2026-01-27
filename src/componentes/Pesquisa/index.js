29be9c05f1b6e72502c054bf44e5bbed7f8147a2
Hariel Santos <harielp.santos1@gmail.com>
2026-01-27 16:56:56 -0400

    rotasFavoritos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import Input from "../Input";
 import styled from "styled-components";
 import { useState } from "react";
-import {Livros} from './dadosPesquisa'
+import { useEffect } from "react";
+import { getLivros } from "../../servicos/livros";
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -42,6 +43,16 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+    const [livros, setLivros] = useState([])
+
+    useEffect(() => {
+        fetchLivros();
+    }, []);
+
+    async function fetchLivros(){
+        const livrosDaApi = await getLivros();
+        setLivros(livrosDaApi);
+    }
 
     return (
         <PesquisaContainer>
@@ -51,7 +62,7 @@ function Pesquisa() {
                 placeholder="encontre sua proxima leitura."
                 onBlur={evento => {
                     const textoDigitado = evento.target.value;
-                    const resultadoPesquisa = Livros.filter(livro => livro.nome.includes(textoDigitado));
+                    const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado));
                     setLivrosPesquisados(resultadoPesquisa);
                 }}
             />
---
+++ src/rotas/Favoritos.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { getFavoritos } from '../servicos/favoritos';
+
+
+const AppContainer = styled.div`
+      width: 100vw;
+      height: 100vh;
+      background-image: linear-gradient(90deg, #002F52 35%, #326589);
+
+
+`
+
+function Favoritos() {
+  const [favoritos, setFavoritos] = useState([])
+  
+  async function fetchFavoritos(){
+    const favoritosDaAPI = await getFavoritos();
+    setFavoritos(favoritosDaAPI);
+  }
+
+  useEffect(() => {
+    fetchFavoritos();
+   }, []);
+  return (
+    <AppContainer>
+      {favoritos.map(favorito => (
+        <p>{favorito.nome}</p>
+      ))}
+    </AppContainer>   
+  );
+}
+
+export default Favoritos;
